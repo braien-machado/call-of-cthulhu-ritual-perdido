4fc9031d63490ccb4e69ce800e693b6597d209d3
Braien Machado de Paula <braienmp@outlook.com>
2025-01-03 15:16:26 -0300

    chore: add custom colors to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,20 @@ export default {
   content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      colors: {
+        // Cor principal (60%)
+        'primary': '#121212', // Cinza muito escuro
+
+        // Cor secundária (30%)
+        'secondary': '#4B0082', // Roxo escuro
+
+        // Cor de destaque (10%)
+        'terciary': '#00FF00', // Verde neon
+
+        // Texto comum
+        'text-primary': '#D3D3D3', // Cinza claro
+        'text-secondary': '#A9A9A9', // Cinza escuro
+      },
       fontFamily: {
         sans: [
           '"Inter"',
