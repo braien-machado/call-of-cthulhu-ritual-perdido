0f26ced2fecd369fa9b4d3cc1a6f1a513d2e4b80
Braien Machado de Paula <braienmp@outlook.com>
2025-01-03 16:12:58 -0300

    style: format code using biome

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,10 +1,13 @@
-import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import type { Route } from "./+types/home";
 
-export function meta({}: Route.MetaArgs) {
+export function meta(_: Route.MetaArgs) {
   return [
     { title: "Call of Cthulhu: O Ritual Perdido" },
-    { name: "description", content: "Bem-vindo a Call of Cthulhu: O Ritual Perdido" },
+    {
+      name: "description",
+      content: "Bem-vindo a Call of Cthulhu: O Ritual Perdido",
+    },
   ];
 }
 
--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -4,20 +4,16 @@ export function Welcome() {
   return (
     <main className="relative flex items-center justify-center h-screen w-full max-w-7xl mx-auto">
       <div className="w-full flex flex-col gap-10 absolute p-4 sm:static bg-primary/70 sm:bg-transparent">
-        <header >
-          <h1 className="text-4xl text-center text-terciary">Call of Cthulhu: O Ritual Perdido</h1>
+        <header>
+          <h1 className="text-4xl text-center text-terciary">
+            Call of Cthulhu: O Ritual Perdido
+          </h1>
         </header>
         <nav className="flex flex-col items-center justify-center gap-4">
           {resources.map((resource) => (
-            <a
-              key={resource.href}
-              href={resource.href}
-              className="text-lg"
-            >
+            <a key={resource.href} href={resource.href} className="text-lg">
               <div className="bg-secondary px-4 py-2 rounded flex items-center justify-center">
-                <span>
-                  {resource.text}
-                </span>
+                <span>{resource.text}</span>
               </div>
             </a>
           ))}
